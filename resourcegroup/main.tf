879dde173e4a4aea16810f89f542b8c52003f37d
Harish Dharmavaram (CW) <Harish.Dharmavaram@jda.com>
2019-08-21 18:09:37 +0530

    backednd change

--- resourcegroup/main.tf
+++ resourcegroup/main.tf
@@ -2,10 +2,10 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name  = "jdastorage"
-	resource_group_name   = "backendstrg"
-    container_name        = "statetf"
-    key                   = "rg.tfstate"
+    storage_account_name  = "jdateststrg"
+	resource_group_name   = "terraform-rg"
+    container_name        = "terragruntversion"
+    key                   = "terraform.tfstate"
   }
 }
 resource "azurerm_resource_group" "rgtest" {
